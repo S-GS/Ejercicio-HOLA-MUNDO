b8f396fed483981ad307c7b42980ea9574dc01b1
S-GS <salvigarcia1405@gmail.com>
2024-09-03 16:58:32 +0200

    S-SG.py

--- Ej 1 HOLA MUNDO.py
+++ Ej 1 HOLA MUNDO.py
@@ -10,4 +10,11 @@ ESTO TAMBIÉN ES UN COMENTARIO EN VARIAS LINEAS
 var ("numero")
 my_variable = "Mi variable"
 bool ("día")
-const
+MY_CONSTANT = "Mi constante"
+my_int = 1
+my_float = 1,3
+my_bool = True
+my_bool = false
+my_string = "Cadena de Texto"
+my_string = 'Otra cadena de texto"
+print "!Hola, Python."
